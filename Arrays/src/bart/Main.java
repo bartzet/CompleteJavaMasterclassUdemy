1fe41cebd1154079c45847ee1f02683a0b8370ab
Dell-Komputer\Dell <ziembabartek@gmail.com>
2017-11-01 21:41:35 +0100

    Arrays

---
+++ Arrays/src/bart/Main.java
@@ -0,0 +1,16 @@
+package bart;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntArray[] = new int[10]; //Array with 10 elements
+        myIntArray[5] = 50;
+        System.out.println("6th element of myIntArray is: " + myIntArray[5]);
+
+        double myDoubleArray[] = new double[10];
+        myDoubleArray[0] = 11.3;
+        System.out.println("1st element of myDoubleArray is " + myDoubleArray[0]);
+
+
+    }
+}
