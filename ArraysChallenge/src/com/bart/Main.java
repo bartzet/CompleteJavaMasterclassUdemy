0c1deee02c7c44bea4abfb20a2c64279ad0dc2fe
Bart Ziemba <bart@novoda.com>
2017-11-06 08:14:36 +0000

    Sort array

--- ArraysChallenge/src/com/bart/Main.java
+++ ArraysChallenge/src/com/bart/Main.java
@@ -33,11 +33,26 @@ public class Main {
     }
 
     //copy elemetnes from other array and sort them
-    public static int sortIntegets(int[] array) {
+    public static int[] sortIntegets(int[] array) {
         int[] sortedArray = new int[array.length];
         for (int i=0; i<array.length; i++) {
             sortedArray[i] = array[i];
         }
+        boolean flag = true;
+        int temp;
+
+        while (flag) {
+            flag = false;
+            for (int i=0; i<sortedArray.length-1; i++) {
+                if (sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
     }
 
 
