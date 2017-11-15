6a503e86fab863d16ff59c00343a5187c0f21c94
Dell-Komputer\Dell <ziembabartek@gmail.com>
2017-11-15 09:22:19 +0100

    ArrayListMain

--- ArrayList/src/bart/Main.java
+++ ArrayList/src/bart/Main.java
@@ -1,6 +1,11 @@
 package bart;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
 
     public static void main(String[] args) {
 	// write your code here
