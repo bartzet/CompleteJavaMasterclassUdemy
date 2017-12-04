5d044261ea29bc7628887701e9c2fdfb9f3f9ea5
Bart Ziemba <bart@novoda.com>
2017-12-04 16:27:42 +0000

    AutoboxingAndUnboxingChallenge

--- AutoboxingAndUnboxingChallenge/src/bart/Bank.java
+++ AutoboxingAndUnboxingChallenge/src/bart/Bank.java
@@ -1,7 +1,67 @@
 package bart;
 
+import java.util.ArrayList;
+
 /**
- * Created by Dell on 2017-11-27.
+ * Created by bartziemba on 01/12/2017.
  */
 public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public Boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+
+        return false;
+    }
+
+    public Boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.addCustomerTransaction(customerName, amount);
+        }
+
+        return false;
+    }
+
+    private Branch findBranch(String branchName) {
+        for (int i = 0; i < this.branches.size(); i++) {
+            Branch checkedBranch = this.branches.get(i);
+            if (checkedBranch.getName().equals(branchName)) {
+                return checkedBranch;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean listCustomers(String branchName, boolean showTransactions) {
+        Branch branch = findBranch(branchName);
+            if (branch != null) {
+                System.out.println("Customers details for branch " + branch.getName());
+
+                ArrayList<Customer> branchCustomers = branch.getCustomers();
+                for (int i=0; i<branchCustomers.size(); i++) {
+                    System.out.println("Customer " );
+                }
+            }
+
+        return false;
+    }
 }
